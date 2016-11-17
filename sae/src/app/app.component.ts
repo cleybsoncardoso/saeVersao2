029010bd096fa111bcbc72b86753c3ca1a7d779a
cleybson cardoso <cleybsonc@gmail.com>
2016-11-17 01:17:18 -0200

    escolher dias

--- sae/src/app/app.component.ts
+++ sae/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 import { PacientesPage } from '../pages/pacientes/pacientes';
 import { AlterarSenhaPage } from '../pages/alterar-senha/alterar-senha';
-import {AprazamentoPage}from '../pages/aprazamento/aprazamento';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,7 +14,7 @@ export class MyApp {
   private alterarsenha:any = AlterarSenhaPage;
   private home:any = HomePage;
   private pacientes:any = PacientesPage;
-  rootPage: any = AprazamentoPage;
+  rootPage: any = HomePage;
 
   constructor(public platform: Platform, public menu: MenuController) {
     this.initializeApp();
