12bfc25230c9c6837c77b4d775219ed472b791b4
Lucas <lukspaiva@hotmail.com>
2017-07-18 02:33:35 -0300

    api editar historico

--- api/editHistorico.php
+++ api/editHistorico.php
@@ -14,7 +14,8 @@
         $motivoInternacao            = $request->motivoInternacao;
         $antecedentes                = $request->antecedentes;
         $alergias                    = $request->alergias;
-		$aspiracao                   = $request->aspiracao;
+		$aspiracaoQuantidade         = $request->aspiracaoQuantidade;
+		$aspiracaoCaracteristica     = $request->aspiracaoCaracteristica;
         $vacinas                     = $request->vacinas;
         $consciencia                 = $request->consciencia;
         $glasgow                     = $request->glasgow;
@@ -48,13 +49,15 @@
             $drenagemToracicaDTD = 0;            
         }
 		
-        $drenagemToracica				 = $request->drenagemToracica;
+		$drenagemQuantidade  			 = $request->drenagemQuantidade;
+        $drenagemCaracteristica			 = $request->drenagemCaracteristica;
         $mamas							 = $request->mamas;
         $avaliacaoCardiovascular_Fc 	 = $request->avaliacaoCardiovascular_Fc;
         $avaliacaoCardiovascular_Pa 	 = $request->avaliacaoCardiovascular_Pa;
         $avaliacaoCardiovascular_PVC	 = $request->avaliacaoCardiovascular_PVC;
         $avaliacaoCardiovascular_PAM	 = $request->avaliacaoCardiovascular_PAM;
         $pulso							 = $request->pulso;
+		$pulsoPalpabilidade				 = $request->pulsoPalpabilidade;
         $presencaDeEdema				 = $request->presencaDeEdema;
         $turgidezDaPele					 = $request->turgidezDaPele;
         $eliminacaoUrinaria_Volume		 = $request->eliminacaoUrinaria_Volume;
@@ -66,19 +69,17 @@
         $presencaoDe					 = $request->presencaoDe;
         $abdome							 = $request->abdome;
         $RHA							 = $request->RHA;
-        $ostomia						 = $request->ostomia;
+		$ostomiaLocal					 = $request->ostomiaLocal;
+        $ostomiaCaracteristica			 = $request->ostomiaCaracteristica;
         $eliminacaoIntestinal			 = $request->eliminacaoIntestinal;
         $eliminacaoIntestinal_frequencia = $request->eliminacaoIntestinal_frequencia;
         $pele							 = $request->pele;
         $pele_temperatura				 = $request->pele_temperatura;
         $olhos							 = $request->olhos;
-        $AVP							 = $request->AVP;
         $AVP_local						 = $request->AVP_local;
 		$AVP_tempo						 = $request->AVP_tempo;
-        $CVC        					 = $request->CVC;
         $CVC_local						 = $request->CVC_local;
 		$CVC_tempo						 = $request->CVC_tempo;
-        $dreno							 = $request->dreno;
         $dreno_local					 = $request->dreno_local;
         $dreno_tipo						 = $request->dreno_tipo;
         $genitalia			 			 = $request->genitalia;
@@ -90,82 +91,83 @@
 
         $sql = "SELECT * FROM saeapp_historico WHERE idsaeapp_historico = '$idsaeapp_historico'";
         $result = $con->query($sql);
-        $num = $result->num_rows;
+        $numrow = $result->num_rows;
 		
 		if ($numrow == 1) {
-			$sql = "UPDATE saeapp_historico (
-				internacoes = '$internacoes',
-				motivoInternacao = '$motivoInternacao',
-				antecedentes = '$antecedentes',
-				alergias = '$alergias',
-				aspiracao = '$aspiracao',
-				vacinas = '$vacinas',
-				consciencia = '$consciencia',
-				glasgow = '$glasgow',
-				pupilas = '$pupilas',
-				mmii_esquerdo = '$mmii_esquerdo',
-				mmii_direito = '$mmii_direito',
-				mmss_esquerdo = '$mmss_esquerdo',
-				mmss_direito = '$mmss_direito',
-				falaELinguagem = '$falaELinguagem',
-				O2 = '$O2',
-				SpO = '$SpO',
-				FR = '$FR',
-				oxigenacao = '$oxigenacao',
-				modalidade = '$modalidade',
-				FiO2 = '$FiO2',
-				Peep = '$Peep',
-				SpO2 = '$SpO2',
-				auscultaPulmonar_MvPresente = '$auscultaPulmonar_MvPresente',
-				auscultaPulmonar_Ruidos = '$auscultaPulmonar_Ruidos',
-				prevencaoDeTosse = '$prevencaoDeTosse',
-				presencaoDeTosse_xpectoracao = '$presencaoDeTosse_xpectoracao',
-				drenagemToracicaDTE = '$drenagemToracicaDTE',
-				drenagemToracicaDTD = '$drenagemToracicaDTD',
-				drenagemToracica '$drenagemToracica',
-				mamas = '$mamas',
-				avaliacaoCardiovascular_Fc = '$avaliacaoCardiovascular_Fc', 
-				avaliacaoCardiovascular_Pa = '$avaliacaoCardiovascular_Pa',
-				avaliacaoCardiovascular_PVC = '$avaliacaoCardiovascular_PVC',
-				avaliacaoCardiovascular_PAM = '$avaliacaoCardiovascular_PAM',
-				pulso = '$pulso',
-				presencaDeEdema = '$presencaDeEdema',
-				turgidezDaPele = '$turgidezDaPele',
-				eliminacaoUrinaria_Volume = '$eliminacaoUrinaria_Volume',
-				eliminacaoUrinaria = '$eliminacaoUrinaria',
-				hidratacao_Caracteristicas = '$hidratacao_Caracteristicas',
-				tipoDeDieta = '$tipoDeDieta',
-				glicemia = '$glicemia',
-				alimentacao_ViasDeAdministracao = '$alimentacao_ViasDeAdministracao',
-				presencaoDe = '$presencaoDe',
-				abdome = '$abdome',
-				RHA = '$RHA',
-				ostomia = '$ostomia',
-				eliminacaoIntestinal = '$eliminacaoIntestinal',
-				eliminacaoIntestinal_frequencia = '$eliminacaoIntestinal_frequencia',
-				pele = '$pele', 
-				pele_temperatura = '$pele_temperatura',
-				olhos = '$olhos',
-				AVP = '$AVP',
-				AVP_local = '$AVP_local',
-				AVP_tempo = '$AVP_tempo',
-				CVC = '$CVC',
-				CVC_local = '$CVC_local',
-				CVC_tempo = '$CVC_tempo',
-				dreno = '$dreno',
-				dreno_local = '$dreno_local',
-				dreno_tipo = '$dreno_tipo',
-				genitalia = '$genitalia',
-				genitalia_lesoes = '$genitalia_lesoes',  
-				necessidadeDeContencao = '$necessidadeDeContencao',
-				riscoParaQueda = '$riscoParaQueda',
-				escore = '$escore', 
-				observacoes = '$observacoes'
-			) 
+			$sql = "UPDATE saeapp_historico set
+				internacoes   				= '$internacoes',
+				motivoInternacao			= '$motivoInternacao',
+				antecedentes				= '$antecedentes',
+				alergias					= '$alergias',
+				aspiracaoQuantidade			= '$aspiracaoQuantidade',
+				aspiracaoCaracteristica		= '$aspiracaoCaracteristica', 
+				vacinas						= '$vacinas',
+				consciencia					= '$consciencia',
+				glasgow						= '$glasgow',
+				pupilas						= '$pupilas', 
+				mmii_esquerdo				= '$mmii_esquerdo',
+				mmii_direito				= '$mmii_direito',
+				mmss_esquerdo				= '$mmss_esquerdo',
+				mmss_direito				= '$mmss_direito',
+				falaELinguagem				= '$falaELinguagem',
+				O2							= '$O2',
+				SpO							= '$SpO',
+				FR							= '$FR',
+				oxigenacao					= '$oxigenacao', 
+				modalidade					= '$modalidade', 
+				FiO2						= '$FiO2',
+				Peep						= '$Peep',
+				SpO2						= '$SpO2',
+				auscultaPulmonar_MvPresente	= '$auscultaPulmonar_MvPresente',
+				auscultaPulmonar_Ruidos		= '$auscultaPulmonar_Ruidos', 
+				prevencaoDeTosse			= '$prevencaoDeTosse', 
+				presencaoDeTosse_xpectoracao= '$presencaoDeTosse_xpectoracao', 
+				drenagemToracicaDTE			= '$drenagemToracicaDTE', 
+				drenagemToracicaDTD			= '$drenagemToracicaDTD',
+				drenagemQuantidade			= '$drenagemQuantidade',
+				drenagemCaracteristica		= '$drenagemCaracteristica', 
+				mamas						= '$mamas', 
+				avaliacaoCardiovascular_Fc	= '$avaliacaoCardiovascular_Fc', 
+				avaliacaoCardiovascular_Pa	= '$avaliacaoCardiovascular_Pa',
+				avaliacaoCardiovascular_PVC	= '$avaliacaoCardiovascular_PVC', 
+				avaliacaoCardiovascular_PAM	= '$avaliacaoCardiovascular_PAM',
+				pulso						= '$pulso',
+				pulsoPalpabilidade			=	'$pulsoPalpabilidade',
+				presencaDeEdema 			= '$presencaDeEdema',
+				turgidezDaPele				=	'$turgidezDaPele',
+				eliminacaoUrinaria_Volume	= 	'$eliminacaoUrinaria_Volume', 
+				eliminacaoUrinaria			=	'$eliminacaoUrinaria',
+				hidratacao_Caracteristicas	= '$hidratacao_Caracteristicas',
+				tipoDeDieta					= '$tipoDeDieta', 
+				glicemia					= '$glicemia', 
+				alimentacao_ViasDeAdministracao	= '$alimentacao_ViasDeAdministracao', 
+				presencaoDe						= '$presencaoDe',
+				abdome							= '$abdome',
+				RHA								= '$RHA',
+				ostomiaLocal					= '$ostomiaLocal',
+				ostomiaCaracteristica			= '$ostomiaCaracteristica',
+				eliminacaoIntestinal			= '$eliminacaoIntestinal', 
+				eliminacaoIntestinal_frequencia = '$eliminacaoIntestinal_frequencia', 
+				pele							= '$pele', 
+				pele_temperatura				= '$pele_temperatura',
+				olhos							= '$olhos',
+				AVP_local						= '$AVP_local',
+				AVP_tempo						= '$AVP_tempo',
+				CVC_local						= '$CVC_local',
+				CVC_tempo						= '$CVC_tempo',
+				dreno_local						= '$dreno_local', 
+				dreno_tipo						= '$dreno_tipo',
+				genitalia						= '$genitalia',
+				genitalia_lesoes				= '$genitalia_lesoes', 
+				necessidadeDeContencao			= '$necessidadeDeContencao',
+				riscoParaQueda					= '$riscoParaQueda',
+				escore							= '$escore', 
+				observacoes						= '$observacoes'
+				
 			WHERE idsaeapp_historico = '$idsaeapp_historico'";
 			$con->query($sql);
 			
-			echo json_encode(true);
+			echo json_encode($sql);
 			
 		} else {
 			echo json_encode(false);
